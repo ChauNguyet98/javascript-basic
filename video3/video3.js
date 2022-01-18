890438145402cbb9bf61130d3b925e97ab546176
ChauNguyet98 <chaunguyet1998@gmail.com>
2022-01-18 20:51:13 +0700

    source code chapter 2, video 3(#7)

---
+++ video3/video3.js
@@ -0,0 +1,8 @@
+// const x = 5;
+// x = 6;
+console.log(`Hello' '" 'World from JavaScript: `);
+
+let x = 5, y = 6, z = 7;
+console.log("Test String", 'x = ', x, 'y = ', y, 'z = ', z);
+
+console.log("Test String2: " + 'x = ' + x + 'y = ' + y + 'z = ' + z);
